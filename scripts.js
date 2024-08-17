2956aa3b0890345181e69884d6c129d636f87674
1davidmahoney <53376261+1davidmahoney@users.noreply.github.com>
2024-08-17 00:39:43 -0400

    Trying to do light/dark favicon depending on user's browser theme.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
-// Add any JavaScript you need here
+function setFavicon(lightMode) {
+    var favicon = document.querySelector('link[rel="icon"]');
+    
+    if (!favicon) {
+        favicon = document.createElement('link');
+        favicon.rel = 'icon';
+        document.head.appendChild(favicon);
+    }
+    
+    if (lightMode.matches) {
+        favicon.href = '/images/favicon/favicon-dark.ico';  // Favicon for light mode
+    } else {
+        favicon.href = '/images/favicon/favicon-light.ico'; // Favicon for dark mode
+    }
+}
+
+// Set favicon on initial load
+var lightMode = window.matchMedia('(prefers-color-scheme: light)');
+setFavicon(lightMode);
+
+// Update favicon when the theme changes
+lightMode.addEventListener('change', () => setFavicon(lightMode));
