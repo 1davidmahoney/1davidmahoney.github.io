1aa0d4066064e15ee810666b9722ad9d928e85f4
1davidmahoney <53376261+1davidmahoney@users.noreply.github.com>
2024-10-17 22:01:00 -0400

    Activity icons; header bounce fix

--- vamnj/script.js
+++ vamnj/script.js
@@ -12,15 +12,23 @@ updateViewportSize(); // Initialize on page load
 let lastScrollTop = 0;
 const header = document.querySelector('header');
 const headerHeight = header.offsetHeight;
+let isScrolling = false;
 
 window.addEventListener('scroll', function() {
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    if (scrollTop > lastScrollTop && scrollTop > headerHeight) {
-        // Scroll down past header height
-        header.style.top = '-82px';
-    } else if (scrollTop < lastScrollTop) {
-        // Scroll up
-        header.style.top = '0';
+
+    if (!isScrolling) {
+        window.requestAnimationFrame(() => {
+            if (scrollTop > lastScrollTop && scrollTop > headerHeight) {
+                // Scroll down past header height
+                header.style.top = '-100px';
+            } else if (scrollTop < lastScrollTop && scrollTop > 0) {
+                // Scroll up but not at the very top of the page
+                header.style.top = '0';
+            }
+            lastScrollTop = scrollTop;
+            isScrolling = false;
+        });
+        isScrolling = true;
     }
-    lastScrollTop = scrollTop;
 });
