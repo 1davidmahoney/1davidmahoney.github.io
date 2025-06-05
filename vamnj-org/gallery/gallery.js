ab9e15058245bffc9c1f7994fd56db6871170371
1davidmahoney <53376261+1davidmahoney@users.noreply.github.com>
2025-06-05 18:34:05 -0400

    Gallery polish

--- vamnj-org/gallery/gallery.js
+++ vamnj-org/gallery/gallery.js
@@ -1,12 +1,18 @@
 const images = [
     ['/vamnj-org/gallery/images/current-display/01.jpg', '/vamnj-org/gallery/images/current-display/02.jpg', '/vamnj-org/gallery/images/current-display/03.jpg', '/vamnj-org/gallery/images/current-display/04.jpg', '/vamnj-org/gallery/images/current-display/05.jpg', '/vamnj-org/gallery/images/current-display/06.jpg'],
     ['/vamnj-org/gallery/images/pickup-trucks/01.jpg', '/vamnj-org/gallery/images/pickup-trucks/02.jpg', '/vamnj-org/gallery/images/pickup-trucks/03.jpg', '/vamnj-org/gallery/images/pickup-trucks/04.jpg'],
-    ['/vamnj-org/gallery/images/stock-cars/01.jpg', '/vamnj-org/gallery/images/stock-cars/02.jpg', '/vamnj-org/gallery/images/stock-cars/03.jpg', '/vamnj-org/gallery/images/stock-cars/04.jpg'],
-    ['/vamnj-org/gallery/images/50s-60s/01.jpg', '/vamnj-org/gallery/images/50s-60s/02.jpg', '/vamnj-org/gallery/images/50s-60s/03.jpg', '/vamnj-org/gallery/images/50s-60s/04.jpg', '/vamnj-org/gallery/images/50s-60s/05.jpg', '/vamnj-org/gallery/images/50s-60s/06.jpg']
+    ['/vamnj-org/gallery/images/stock-cars/01.jpg', '/vamnj-org/gallery/images/stock-cars/02.jpg', '/vamnj-org/gallery/images/stock-cars/03.jpg', '/vamnj-org/gallery/images/stock-cars/04.jpg', '/vamnj-org/gallery/images/stock-cars/05.jpg', '/vamnj-org/gallery/images/stock-cars/06.jpg'],
+    ['/vamnj-org/gallery/images/50s-60s/01.jpg', '/vamnj-org/gallery/images/50s-60s/02.jpg', '/vamnj-org/gallery/images/50s-60s/03.jpg', '/vamnj-org/gallery/images/50s-60s/04.jpg']
 ];
 
 let currentSetIndex = 0;
 let currentPicIndex = 0;
+
+function handleKey(event) {
+    if (event.key === 'ArrowLeft') { prevImage(event); }
+    else if (event.key === 'ArrowRight') { nextImage(event); }
+    else if (event.key === 'Escape') { closeViewer(); }
+}
 
 function openViewer(setIndex, picIndex) {
     currentSetIndex = setIndex;
@@ -16,6 +22,8 @@ function openViewer(setIndex, picIndex) {
 
     // Disable all scrolling on the page
     document.body.style.overflow = 'hidden';
+    
+    document.addEventListener('keydown', handleKey);
 }
 
 function closeViewer() {
@@ -23,6 +31,8 @@ function closeViewer() {
     
     // Re-enable scrolling on the page
     document.body.style.overflow = '';
+    
+    document.removeEventListener('keydown', handleKey);
 }
 
 function prevImage(event) {
