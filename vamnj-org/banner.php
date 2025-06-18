bf66200574ba06c62f1dea933193c5baec8b1cdf
1davidmahoney <53376261+1davidmahoney@users.noreply.github.com>
2025-06-18 15:32:10 -0400

    Update banner.php

--- vamnj-org/banner.php
+++ vamnj-org/banner.php
@@ -1,6 +1,6 @@
 <!-- header.php -->
-<div id="notification-banner" class="banner">
-    <span class="banner-text">Museum closed June 20 for renovations.</span>
+<div id="notification-banner" class="banner hidden-by-default">
+    <span class="banner-text">The annual raffle is underway! Don't forget to <a href="/raffle/">buy tickets</a>!</span>
     <button class="banner-dismiss" aria-label="Dismiss notification">&times;</button>
 </div>
 <!-- rest of header ... -->
